8c05b13f9fb6bd41122292cfd0ec08c3a342c9de
tomo-john <keririnkery.kery@gmail.com>
2024-08-07 19:12:00 +0900

    add: cal

--- C/ubuntu/c/calculator.c
+++ C/ubuntu/c/calculator.c
@@ -8,7 +8,34 @@ int main(){
   printf("数字2つと演算子を入力して下さい(例: 2 + 8): ");
   scanf("%f %c %f", &num1, &operator, &num2);
 
-  // c
+  // チェック
+  printf("入力された式: %.2f %c %.2f\n", num1, operator, num2);
+
+  float result;
+
+  switch(operator){
+    case '+':
+      result = num1 + num2;
+      break;
+    
+    case '-':
+      result = num1 - num2;
+      break;
+
+    case '*':
+      result = num1 * num2;
+      break;
+
+    case '/':
+      result = num1 / num2;
+      break;
+
+    default:
+      printf("演算子の入力が間違っています: %c\n", operator);
+      return 0;
+  }
+
+  printf("計算結果: %.2f\n", result);
 
   return 0;
 }
