c2c05d5a4ee58d3d446c18990764e16027ad3f97
tomo-john <keririnkery.kery@gmail.com>
2024-08-07 12:57:12 +0900

    add: cal

---
+++ C/ubuntu/c/calculator.c
@@ -0,0 +1,14 @@
+// 電卓
+#include <stdio.h>
+
+int main(){
+  float num1, num2;
+  char operator;
+
+  printf("数字2つと演算子を入力して下さい(例: 2 + 8): ");
+  scanf("%f %c %f", &num1, &operator, &num2);
+
+  // c
+
+  return 0;
+}
