14c85eabd40ecd0ba2be1d370b609c677133c861
tomo-john <keririnkery.kery@gmail.com>
2024-08-19 07:52:42 +0900

    add: sample

---
+++ C/sample_code/ky207.c
@@ -0,0 +1,28 @@
+// 代入された数値を表示
+
+#include <stdio.h>
+
+int main(void){
+  int n1, n2;
+
+  n1 = 1;
+
+  printf("現在の変数n1の内容: %d\n", n1);
+
+  n1 = 2;
+
+
+  printf("現在の変数n1の内容: %d\n", n1);
+
+  printf("変数n2の値を入力してください: ");
+  scanf("%d", &n2);
+
+  printf("現在の変数n1の内容: %d\n", n1);
+  printf("現在の変数n2の内容: %d\n", n2);
+
+  n1 = n2;
+
+  printf("現在の変数n1の内容: %d\n", n1);
+  printf("現在の変数n2の内容: %d\n", n2);
+
+}
