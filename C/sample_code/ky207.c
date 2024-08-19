107286e61c8ab665d5d30acd1f10a857552d2845
tomo-john <keririnkery.kery@gmail.com>
2024-08-20 05:27:06 +0900

    add: sample

--- C/sample_code/ky207.c
+++ C/sample_code/ky207.c
@@ -11,8 +11,7 @@ int main(void){
 
   n1 = 2;
 
-
-  printf("現在の変数n1の内容: %d\n", n1);
+  printf("現在の変数n2の内容: %d\n", n1);
 
   printf("変数n2の値を入力してください: ");
   scanf("%d", &n2);
@@ -20,6 +19,7 @@ int main(void){
   printf("現在の変数n1の内容: %d\n", n1);
   printf("現在の変数n2の内容: %d\n", n2);
 
+  printf("n1 = n2 を実施\n");
   n1 = n2;
 
   printf("現在の変数n1の内容: %d\n", n1);
