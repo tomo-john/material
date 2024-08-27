290a71948eeca1672eab59dd17e90b7684d3b1e7
tomo-john <keririnkery.kery@gmail.com>
2024-08-27 17:35:21 +0900

    add: sample

--- C/sample_code/format.c
+++ C/sample_code/format.c
@@ -11,4 +11,20 @@ int main(void){
 
   printf("[%f]\n", 123.45678);       // [123.456780]
   printf("[%8.3f]\n", 123.45678);    // [ 123.457]
+
+  printf("%s\n", "-----------------------------------------------------------------------------------");
+
+  /* unsigned は符号なしの整数 */
+  unsigned int a = 0b1101; // 10進数の13
+  unsigned int b = 0b1011; // 10進数の11
+
+  printf("a: %08d\n", a);
+  printf("b: %08d\n", b);
+
+  printf("a & b  = %08d\n", a & b);
+  printf("a | b  = %08d\n", a | b);
+  printf("a ^ b  = %08d\n", a ^ b);
+  printf("~a     = %08d\n", ~a);
+  printf("a << 1 = %08d\n", a << 1);
+  printf("a >> 1 = %08d\n", a >> 1);
 }
