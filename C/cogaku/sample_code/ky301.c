9ba3ba954124a296f82aa540018d61413bfbdb83
tomo-john <keririnkery.kery@gmail.com>
2024-08-28 09:58:01 +0900

    add: c

--- C/cogaku/sample_code/ky301.c
+++ C/cogaku/sample_code/ky301.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int main(void){
-  char sz1[50] = {'H', 'e', 'l', 'l', '0', '\0'};
+  char sz1[50] = {'H', 'e', 'l', 'l', 'o', '\0'};
   printf("%s\n", sz1);
 
   printf("名前を入力してください(MAX49バイト): ");
   scanf("%s", sz1 );
 
-  printf("あなたの名前は %sさんですね\n", sz1);
+  printf("あなたの名前は %s さんですね\n", sz1);
 }
