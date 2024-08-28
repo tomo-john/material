77f5835bae64dad45927af18266178ca2b90f5e8
tomo-john <keririnkery.kery@gmail.com>
2024-08-28 09:26:08 +0900

    add: sample

---
+++ C/cogaku/sample_code/ky304.c
@@ -0,0 +1,14 @@
+// ポインタとアドレス
+#include <stdio.h>
+
+void main(void);
+
+void main(void){
+  char sz1[50] = {'H', 'e', 'l', 'l', 'o', '\0'};
+
+  printf("%s\n", sz1);      // sz1の内容
+
+  printf("%x\n", &sz1[0]);  // sz1の1番目のアドレス
+
+  printf("%x\n", sz1);      // sz1のポインタ
+}
