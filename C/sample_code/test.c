045ab1418543f8e542415703ad8a08fc7c38396a
tomo-john <keririnkery.kery@gmail.com>
2024-08-29 12:12:14 +0900

    add: sample

--- C/sample_code/test.c
+++ C/sample_code/test.c
@@ -2,22 +2,11 @@
 #include <stdio.h>
 
 int main(void){
-  char c1[] = "john";
-  char c2[] = {'j', 'o', 'h', 'n', '\0'};
+  int a = 10;  // aという変数を作成し、10を代入
+  int *p = &a; // ポインタpにaのアドレスを代入
 
-  printf("c1: %s\n", c1); // john
-  printf("c2: %s\n", c2); // john
-
-  printf("c2[2] : %c\n", c2[2]);  // h
-  printf("&c2[2]: %s\n", &c2[2]); // hn => c2[2]以降が表示される
-
-  char *p1 = &c2[0];
-  char *p2 = &c2[2];
-
-  printf("&c2[0] のアドレスの値: %s\n", p1); // john
-  printf("&c2[2] のアドレスの値: %s\n", p2); // hn
-
-  printf("&c2 のアドレス       : %p\n", &c2);    // 0x7fffe660ff73
-  printf("&c2[0] のアドレス    : %p\n", &c2[0]); // 0x7fffe660ff73
-  printf("&c2[2] のアドレス    : %p\n", &c2[2]); // 0x7fffe660ff75
+  printf("%d\n", a);  // 10
+  printf("%p\n", &a); // 0x7ffeb3a82f5c : aのアドレス
+  printf("%p\n", p);  // 0x7ffeb3a82f5c : pに格納されたアドレス(aのアドレス)
+  printf("%d\n", *p); // 10 : pが指している値 = aの値
 }
