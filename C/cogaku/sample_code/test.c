211790dedf79c5ef56bb31fc2c4afe784b6ac75a
tomo-john <keririnkery.kery@gmail.com>
2024-09-01 13:55:37 +0900

    add: sampe

--- C/cogaku/sample_code/test.c
+++ C/cogaku/sample_code/test.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main(){
-  char *p1;
-  p1 = "ABCDEF";
+  int n1[] = {1, 2, 3};
 
-  printf("p1: %s\n", p1);
-  printf("*p1(%%p): %p\n", (void*)p1);
-  printf("*p1(%%c): %c\n", *p1);
+  printf("%d\n", *n1);
+  printf("%d\n", *(n1 + 1));
+  printf("%d\n", *(n1 + 2));
 
   return 0;
 }
