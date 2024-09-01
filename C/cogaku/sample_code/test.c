3811ecacf19b5a6a57636f803067190bcdcb58f8
tomo-john <keririnkery.kery@gmail.com>
2024-09-01 14:27:54 +0900

    add: sample

--- C/cogaku/sample_code/test.c
+++ C/cogaku/sample_code/test.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
-int main(){
-  int n1[] = {1, 2, 3};
+// 構造体の定義
+struct DOG {
+    char name[50];
+    int age;
+};
 
-  printf("%d\n", *n1);
-  printf("%d\n", *(n1 + 1));
-  printf("%d\n", *(n1 + 2));
+int main() {
+    struct DOG myDog = {"john", 10};       // 構造体変数の宣言と初期化
+    struct DOG *myDogPtr = &myDog;         // 構造体ポインタの宣言と初期化
 
-  return 0;
+    // 直接アクセス
+    printf("名前: %s\n", myDog.name); // john
+    printf("年齢: %d\n", myDog.age);  // 10
+
+    // ポインタを使ったアクセス
+    printf("名前: %s\n", myDogPtr->name); // john
+    printf("年齢: %d\n", myDogPtr->age);  // 10
+
+    return 0;
 }
