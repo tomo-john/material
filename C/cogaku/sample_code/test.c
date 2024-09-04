fae55c1763886151a793dd4c3256ef435c9f421d
tomo-john <keririnkery.kery@gmail.com>
2024-09-04 13:08:40 +0900

    add: sample

--- C/cogaku/sample_code/test.c
+++ C/cogaku/sample_code/test.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 struct Animal {
-    char type; // 'd' for dog, 'c' for cat
-    union {
-        int dogAge;  // For dogs, we store age
-        int catLives; // For cats, we store remaining lives
-    } info;
+  char type;
+  union {
+    int dogAge;
+    int rabbitAge;
+  } info;
 };
 
 int main() {
-    struct Animal myPet;
+  struct Animal myPet;
 
-    // 犬を設定
-    myPet.type = 'd';
-    myPet.info.dogAge = 5; // 犬の年齢を5歳に設定
-    printf("Animal type: %c, Age: %d\n", myPet.type, myPet.info.dogAge);
+  // dogを設定
+  myPet.type = 'd';
+  myPet.info.dogAge = 2;
+  printf("Animal type: %c, Age: %d\n", myPet.type, myPet.info.dogAge);
 
-    // 同じメモリ領域を使用して猫の情報を設定
-    myPet.type = 'c';
-    myPet.info.catLives = 9; // 猫の残りの命を9に設定
-    printf("Animal type: %c, Lives: %d\n", myPet.type, myPet.info.catLives);
+  // 同じメモリ領域を使用してrabbitを設定
+  myPet.type = 'r';
+  myPet.info.rabbitAge = 10;
+  printf("Animal type: %c, Age: %d\n", myPet.type, myPet.info.rabbitAge);
 
-    return 0;
+  return 0;
 }
 
