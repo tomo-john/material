36abe0c91c5227f77e71a3c15ef926ec22f3bbeb
tomo-john <keririnkery.kery@gmail.com>
2024-09-10 08:15:47 +0900

    add: sample

---
+++ C/cogaku/sample_code/ky302.c
@@ -0,0 +1,24 @@
+// switch
+#include <stdio.h>
+
+int main(){
+  int n1;
+
+  printf("0-9を入力: ");
+  scanf("%d", &n1);
+
+  switch(n1){
+    case 0:
+      printf("0john\n");
+      break;
+    case 1:
+      printf("1john\n");
+      break;
+    case 2:
+      printf("2john\n");
+      break;
+    default:
+      printf("0, 1, 2以外john\n");
+      break;
+  }
+}
