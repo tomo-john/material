a7fa07100da1aba629006bbe63c28be8f29e757f
tomo-john <keririnkery.kery@gmail.com>
2024-09-09 12:37:55 +0900

    add: c

--- C/cogaku/sample_code/ohter/test.c
+++ C/cogaku/sample_code/ohter/test.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
-void Function1(void){
-  printf("john\n");
-}
+int function(int n);
 
 int main(){
-  Function1();
+  int n1;
+  int n2;
+  n1 = 1;
+
+  n2 = function(n1);
+
+  printf("n2: %d\n", n2);
+}
+
+int function(int n){
+  n += 1;
+  return n;
 }
