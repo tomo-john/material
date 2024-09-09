b46520ae320fedf5fc7b6b76a1f639ef91dd9dea
tomo-john <keririnkery.kery@gmail.com>
2024-09-10 08:54:51 +0900

    add: sample

--- C/cogaku/sample_code/ohter/test.c
+++ C/cogaku/sample_code/ohter/test.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 
-int function(int n);
-
 int main(){
-  int n1;
-  int n2;
+  char c1;
+  printf("英数字1文字を入力: ");
+  scanf("%c", &c1);
 
-  printf("数値を入力: ");
-  scanf("%d", &n1);
-
-  n2 = function(n1);
-
-  printf("n2: %d\n", n2);
-}
-
-int function(int n){
-  n *= n ;
-  return n;
+  printf("入力された文字 %c の16進数のコードは 0x%02X\n", c1, c1);
 }
