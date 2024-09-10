50ecbb8ab9cebc2585a0cf68dab69a6236d6ef0d
tomo-john <keririnkery.kery@gmail.com>
2024-09-10 15:36:31 +0900

    add: sample

--- C/cogaku/sample_code/test.c
+++ C/cogaku/sample_code/test.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
 
-struct Animal {
-  char type;
-  union {
-    int dogAge;
-    int rabbitAge;
-  } info;
-};
+int main(){
+  int n1;
+  printf("数値を入力して下さい: ");
+  scanf("%d", &n1);
 
-int main() {
-  struct Animal myPet;
-
-  // dogを設定
-  myPet.type = 'd';
-  myPet.info.dogAge = 2;
-  printf("Animal type: %c, Age: %d\n", myPet.type, myPet.info.dogAge);
-
-  // 同じメモリ領域を使用してrabbitを設定
-  myPet.type = 'r';
-  myPet.info.rabbitAge = 10;
-  printf("Animal type: %c, Age: %d\n", myPet.type, myPet.info.rabbitAge);
-
-  return 0;
+  if(n1 % 2 == 0)
+    printf("入力された数値 %d は偶数です\n", n1);
+  else
+    printf("入力された数値 %d は奇数です\n", n1);
 }
-
