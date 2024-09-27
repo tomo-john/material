e7173fa1777460ebfd7f9822f8d10defcc0879c9
tomo-john <keririnkery.kery@gmail.com>
2024-09-27 16:47:10 +0900

    add: c

---
+++ C/cogaku/sample_code/ky207.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+  char *pstr;
+  int ni;
+  int nin;
+
+  nin = atoi(argv[1]);
+
+  for(ni = nin; ni < 20; ni++){
+    pstr = strerror(ni);
+    printf("E=NO=%d->%s\n", ni, pstr);
+  }
+
+  return 0;
+}
