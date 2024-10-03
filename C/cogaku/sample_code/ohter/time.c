cb8f947d0505a7b4be0bb2a656cd9b95e0998ca5
tomo-john <keririnkery.kery@gmail.com>
2024-10-03 17:29:49 +0900

    add: sample

--- C/cogaku/sample_code/ohter/time.c
+++ C/cogaku/sample_code/ohter/time.c
@@ -3,12 +3,17 @@
 
 int main() {
   time_t current_time;
+  char* time_str;
 
   // 現在時刻の取得
   current_time = time(NULL);
 
+  // time_tを文字列に変換
+  time_str = ctime(&current_time);
+
   if(current_time != -1){
     printf("現在の時間(1970年からの経過秒数): %ld\n", current_time);
+    printf("現在の時間(文字列形式): %s\n", time_str);
   }
 
 	return 0;
