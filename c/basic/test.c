9dd1b973438940588ecbf9975b504b154a66598e
tomo-john <keririnkery.kery@gmail.com>
2024-10-21 15:25:58 +0900

    sample

--- c/basic/test.c
+++ c/basic/test.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main(){
-  FILE *file;
-
-  file = fopen("sample.txt", "r");
+  FILE *file = fopen("sample.txt", "r");
 
   if(file == NULL){
     perror("Error opening file");
     return -1;
   }
 
-  int ch = fgetc(file);
-  while(ch != EOF){
-    putchar(ch);
-    ch = fgetc(file);
+  char buffer[256]; // 読込みようのバッファを準備
+
+  while(fgets(buffer, sizeof(buffer), file) != NULL){
+    printf("Read line is: %s\n", buffer);
   }
 
   fclose(file);
