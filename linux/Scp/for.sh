60ac82992d5efda78718e9063d1f3edc16e44859
tomo-john <keririnkery.kery@gmail.com>
2024-10-28 11:19:09 +0900

    test

--- linux/Scp/for.sh
+++ linux/Scp/for.sh
@@ -1,8 +1,39 @@
-for i in 1 2 3 4 5; do
+for i in 1 2 3; do
   echo "john$i"
 done
+echo
 
-for i in {1..10}; do
+for i in {1..4}; do
   echo "dog$i"
 done
+echo
+
+animals="dog rabbit bear"
+
+for animal in $animals; do
+  echo $animal
+done
+echo
+
+for file in $(ls); do
+  echo file: $file
+done
+echo
+
+for file in `ls`; do
+  echo file: $file
+done
+echo
+
+for line in $(cat sample.txt); do
+  echo line: $line
+done
+echo
+
+array=("john", "super_john", "old_john")
+
+for item in "${array[@]}"; do
+  echo "item: $item"
+done
+echo
 
--- linux/Scp/while.sh
+++ linux/Scp/while.sh
@@ -3,3 +3,9 @@ while [ $i -le 5 ]; do
   echo "nubmer is $i"
   i=$((i + 1))
 done
+echo
+
+while read -r line; do
+  echo "行の内容: $line"
+done < sample.txt
+echo
