f2e6716f9255e432ab09a232053136274ac654eb
tomo-john <keririnkery.kery@gmail.com>
2024-10-30 11:03:36 +0900

    test

--- linux/Scp/read.sh
+++ linux/Scp/read.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-echo "何か入力して下さい: "
+echo "No.1 何か入力して下さい: "
 read input
 echo "入力されたのは: $input です"
 echo
 
-read -p "-pオプションでメッセージ表示できる: " input2
+read -p "No.2 -pオプションでメッセージ表示できる: " input2
 echo "$input2"
 echo
 
-read -rp "pathを入力: " path
+read -rp "No.3 pathを入力: " path
 echo "$path"
 echo
+
+echo "No.4 複数の単語を入力(スペース区切り): "
+read -a array
+for e in "${array[@]}"; do
+  echo $e
+done
 
