aa6d4e6c6638a79e790f2ba699be6020b94cbfb6
tomo-john <keririnkery.kery@gmail.com>
2025-01-16 17:10:44 +0900

    js

--- html/js/script.js
+++ html/js/script.js
@@ -1,7 +1,8 @@
 const image = document.getElementById('image');
 const step = 10; // 1回の移動量
-let x = window.innerWidth / 2 - 50;  // 初期位置x
-let y = window.innerHeight / 2 - 50; // 初期位置y
+const imageSize = 100;
+let x = window.innerWidth / 2 - imageSize / 2;  // 初期位置x
+let y = window.innerHeight / 2 - imageSize / 2; // 初期位置y
 
 // 画像位置更新関数
 function updatePosition(){
@@ -13,16 +14,16 @@ function updatePosition(){
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'ArrowUp': // ↑
-      y -= step;
+      if (y > 0 ) y -= step;
       break;
     case 'ArrowDown': // ↓
-      y += step;
+      if ( y < window.innerHeight - imageSize ) y += step;
       break;
     case 'ArrowLeft': // ←
-      x -= step;
+      if ( x > 0 ) x -= step;
       break;
     case 'ArrowRight': // →
-      x += step;
+      if ( x < window.innerWidth - imageSize ) x += step;
       break;
   }
   updatePosition();
