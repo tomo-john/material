fb1624740c2f6b7f3965bc171bf8dc7c55a22b31
tomo-john <keririnkery.kery@gmail.com>
2025-01-27 13:17:56 +0900

    Basic

--- linux/Basic/scp/diskmem.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-date    >> diskmem.log
-echo "-----------------------------------------------------" >> diskmem.log
-df -H   >> diskmem.log
-echo "-----------------------------------------------------" >> diskmem.log
-free -m >> diskmem.log
-echo "-----------------------------------------------------" >> diskmem.log
-
