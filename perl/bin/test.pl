1aed3b56b4bba84ea46e3115ab6a1fd989fbb40b
tomo-john <keririnkery.kery@gmail.com>
2025-04-08 11:56:56 +0900

    perl

--- perl/bin/test.pl
+++ perl/bin/test.pl
@@ -1,12 +1,14 @@
 #!/usr/bin/perl
 # perlテスト用
 
-my $filename = "sample.txt";
-open(my $fh, "<", $filename) or die "ファイルを開けません!: $!";
+print "モードを選んで下さい(0: 上書き / 1: 追記): ";
+my $mode = <STDIN>;
+chomp($mode);
 
-while (my $line = <$fh>) {
-  chomp($line);
-  print "読み込み内容: $line\n";
-}
-
+my $open_mode = ($mode eq "0") ? ">" :
+                ($mode eq "1") ? ">>" :
+                die "不正なモードです";
+open(my $fh, $open_mode, "log.txt") or die "File Error: $!";
+print $fh "ログ出力モード: $mode $open_mode\n";
 close($fh);
+
