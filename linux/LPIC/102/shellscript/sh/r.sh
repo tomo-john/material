77a41d2326658d270df0c82d8b7826254f9d6a71
tomo-john <keririnkery.kery@gmail.com>
2025-05-01 20:05:00 +0900

    102

---
+++ linux/LPIC/102/shellscript/sh/r.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo
+echo "START"
+echo "このスクリプトは読み取り権あり、実行権なし"
+echo "bash sourc . コマンドは読み取り権があれば実行可能"
+echo "END"
+
---
+++ linux/LPIC/102/shellscript/sh/x.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo
+echo "START"
+echo "このスクリプトは実行権あり"
+echo "実行権があればスクリプト名での実行が可能($0)"
+echo "END"
+
