5da98ddf8c06f9e4c7aacf1159176b9110a336d4
tomo-john <keririnkery.kery@gmail.com>
2025-07-25 19:35:56 +0900

    php

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>super dog</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+  <div class="container">
+    <div class="circle left"><span>A</span></div>
+    <div class="circle right"><span>B</span></div>
+  </div>
+  
+  <div class="button">
+    <button>押すと爆発</button>
+  </div>
+
+</body>
+</html>
