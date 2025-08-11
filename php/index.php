e21826923d75650a7dfc781e45ec45b60f00a06d
tomo-john <keririnkery.kery@gmail.com>
2025-08-11 16:10:58 +0900

    php

--- php/index.php
+++ php/index.php
@@ -1,3 +1,11 @@
+<?php
+$result = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $type = $_POST['type'] ?? '';
+    $result = "あなたが選んだタイプは: " . htmlspecialchars($type, ENT_QUOTES, 'UTF-8');
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,16 +13,30 @@
   <title>super dog</title>
   <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
 
+  <h2>てすと</h2>
   <div class="container">
     <div class="circle left"><span>A</span></div>
     <div class="circle right"><span>B</span></div>
   </div>
   
-  <div class="button">
-    <button>押すと爆発</button>
+  <div class="select">
+     <form action="" method="post">
+      <label for="type">好きなタイプを選んでね🐶: </label>
+      <select id="type" name="type">
+        <option value = "a">AND</option>
+        <option value = "b">OR</option>
+        <option value = "c">XOR</option>
+      </select>
+      <input type="submit" value="わん!">
+    </form>
   </div>
+
+  <?php if ($result): ?>
+    <P><?php echo $result; ?></p>
+  <?php endif; ?>
 
 </body>
 </html>
