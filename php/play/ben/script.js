14d1767f663763b9f0d71f97a0c8b67dc6ff3934
tomo-john <keririnkery.kery@gmail.com>
2025-08-13 12:00:38 +0900

    play

--- php/play/ben/script.js
+++ php/play/ben/script.js
@@ -2,20 +2,31 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     // HTMLから操作したい要素を取得
-    const colorButton = document.getElementById('color-button');
+    const andButton = document.getElementById('and-button');
+    const orButton = document.getElementById('or-button');
     const resetButton = document.getElementById('reset-button');
-    const svgElement = document.getElementById('interactive-venn');
+    const svgElement = document.getElementById('venn');
 
-    // 「色を付ける」ボタンがクリックされたときの処理
-    colorButton.addEventListener('click', function() {
-        // SVG要素に 'show-intersection' というクラスを追加する
-        svgElement.classList.add('show-intersection');
+    // 「AND」ボタンがクリックされたときの処理
+    andButton.addEventListener('click', function() {
+        // 不要なクラスを削除
+        svgElement.classList.remove('show-or');
+        // SVG要素に 'show-and' というクラスを追加する
+        svgElement.classList.add('show-and');
     });
+
+    // 「OR」ボタンがクリックされたときの処理
+    orButton.addEventListener('click', function() {
+        // 既存のANDクラスは外す
+        svgElement.classList.remove('show-and');
+        // ORクラスを追加
+        svgElement.classList.add('show-or');
+    });;
 
     // 「リセット」ボタンがクリックされたときの処理
     resetButton.addEventListener('click', function() {
-        // SVG要素から 'show-intersection' というクラスを削除する
-        svgElement.classList.remove('show-intersection');
+        // SVG要素から不要なクラスを削除する
+        svgElement.classList.remove('show-and', 'show-or');
     });
     
 });
