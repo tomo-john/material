6ef7d631ec5b807a080682bfb812be0b87269d68
tomo-john <keririnkery.kery@gmail.com>
2025-08-14 08:31:54 +0900

    play

--- php/play/ben/script.js
+++ php/play/ben/script.js
@@ -4,29 +4,34 @@ document.addEventListener('DOMContentLoaded', function() {
     // HTMLから操作したい要素を取得
     const andButton = document.getElementById('and-button');
     const orButton = document.getElementById('or-button');
+    const xorButton = document.getElementById('xor-button');
     const resetButton = document.getElementById('reset-button');
     const svgElement = document.getElementById('venn');
 
-    // 「AND」ボタンがクリックされたときの処理
+    // ANDボタンがクリックされたときの処理
     andButton.addEventListener('click', function() {
         // 不要なクラスを削除
-        svgElement.classList.remove('show-or');
+        svgElement.classList.remove('show-or', 'show-xor');
         // SVG要素に 'show-and' というクラスを追加する
         svgElement.classList.add('show-and');
     });
 
-    // 「OR」ボタンがクリックされたときの処理
+    // ORボタンがクリックされたときの処理
     orButton.addEventListener('click', function() {
-        // 既存のANDクラスは外す
-        svgElement.classList.remove('show-and');
-        // ORクラスを追加
+        svgElement.classList.remove('show-and', 'show-xor');
         svgElement.classList.add('show-or');
-    });;
+    });
+
+    // XORボタンがクリックされたときの処理
+    xorButton.addEventListener('click', function() {
+        svgElement.classList.remove('show-and', 'show-or');
+        svgElement.classList.add('show-xor');
+    });
 
     // 「リセット」ボタンがクリックされたときの処理
     resetButton.addEventListener('click', function() {
         // SVG要素から不要なクラスを削除する
-        svgElement.classList.remove('show-and', 'show-or');
+        svgElement.classList.remove('show-and', 'show-or', 'show-xor');
     });
     
 });
