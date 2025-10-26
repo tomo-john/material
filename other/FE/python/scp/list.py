e0c715373bb377b29cff3bdd47e6b757233cb019
tomo-john <keririnkery.kery@gmail.com>
2025-10-26 16:55:58 +0900

    py

---
+++ other/FE/python/scp/list.py
@@ -0,0 +1,24 @@
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+animals = ["dog", "rabbit", "bear"]
+
+print(animals[0])
+print(animals[-1])
+print(nums[1:4])
+print(len(animals))
+
+print()
+
+name = "じょん"
+level = 99
+
+print("名前:", name, "レベル:", level,"すごい犬")
+
+print(f"私の犬の名前は{name}で、現在{level}レベルです。")
+
+total = 0
+for num in nums:
+  print(num)
+  total += num
+print(f"合計: {total}")
+
