af4c82c3a3488607c9ebd626d21262342ee32abf
tomo-john <keririnkery.kery@gmail.com>
2025-10-26 16:56:04 +0900

    py

--- other/FE/python/scp/test.py
+++ other/FE/python/scp/test.py
@@ -1,24 +1,7 @@
+def gcd(a, b):
+  while b != 0:
+    a, b = b, a % b
+  return a
 
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-animals = ["dog", "rabbit", "bear"]
-
-print(animals[0])
-print(animals[-1])
-print(nums[1:4])
-print(len(animals))
-
-print()
-
-name = "じょん"
-level = 99
-
-print("名前:", name, "レベル:", level,"すごい犬")
-
-print(f"私の犬の名前は{name}で、現在{level}レベルです。")
-
-total = 0
-for num in nums:
-  print(num)
-  total += num
-print(f"合計: {total}")
+print(gcd(18, 12))
 
