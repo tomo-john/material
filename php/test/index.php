4b64c5ea0d34e49f24311cae065f6c82648fc0f7
tomo-john <keririnkery.kery@gmail.com>
2025-11-02 14:13:26 +0900

    test

---
+++ php/test/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP最初の一歩🐶</title>
+</head>
+<body>
+  <h1>
+    <?php
+      // コメント🐶
+      $name = "じょん";
+      $age = 2;
+      echo "こんにちは、" . $name . "🐶!</br>";
+      echo "年齢は " . $age . " 歳だわん";
+    ?>
+  </h1>
+</body>
+</html>
---
+++ php/test/memo.md
@@ -0,0 +1,4 @@
+```
+# .phpの場合PHPサーバー上で実行する
+php -S localhost:8000
+```
