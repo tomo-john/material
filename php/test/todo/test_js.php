05fbd45d97bcc70886460cfa049a284975c48886
tomo-john <keririnkery.kery@gmail.com>
2025-11-21 16:08:22 +0900

    test

--- php/test/todo/test_js.php
+++ php/test/todo/test_js.php
@@ -3,6 +3,11 @@
 $id = $_POST['id'];
 $answer = $_POST['answer'];
 
-echo '$answer: ' . $answer  . ' / $id: ' . $id;
+echo 'このページに辿り着いたあなたは、素晴らしい選択です🐶';
+
+echo '<br><br>';
+echo 'デバッグ用:<br>';
+echo '$_POSTで受け取った$id: ' . $id  . '<br>';
+echo '$_POSTで受け取った$answer: ' . $answer  . '<br>';
 
 ?>
