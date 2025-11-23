16e17e636e3fe6b1c5bce1ce41b2393800278755
tomo-john <keririnkery.kery@gmail.com>
2025-11-24 08:57:48 +0900

    test

---
+++ php/test/dog_point_card_app/index.php
@@ -0,0 +1,27 @@
+<?php
+  
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+  <title>Dog_App🐶</title>
+</head>
+<body>
+  <h2>Dog_App🐶</h2>
+
+  <form action="" method="post">
+    <label for="dog_name">犬の名前:</label>
+    <input id="dog_name" type="text" name="dog_name" placeholder="じょん">
+    <br>
+    <label for="dog_point">今日のポイント:</label>
+    <input id="dog_point" type="number" name="dog_poing" placeholder="10">
+    <br><br>
+    <input type="submit" value="登録🐶">
+  </form>
+
+</body>
+</html>
