f4b1b626183e87a67a4496ecf5734ce911e81539
tomo-john <keririnkery.kery@gmail.com>
2025-11-26 08:28:19 +0900

    test

--- php/test/dogmon_app/index.php
+++ php/test/dogmon_app/index.php
@@ -13,15 +13,17 @@
 </head>
 <body>
 
-  <h2>メインメニュー🐶</h2>
+  <div class="main_menu">
+    <h2>メインメニュー🐶</h2>
 
-  <div class="menu">
-    <ul class="menu_list">
-      <li><a href="#">dogmon作成</a></li>
-      <li><a href="#">dogmon作成</a></li>
-      <li><a href="#">dogmon作成</a></li>
-      <li><a href="#">dogmon作成</a></li>
-    </ul>
+    <div class="menu">
+      <ul class="menu_list">
+        <li><a href="new.php">dogmon作成</a></li>
+        <li><a href="list.php">dogmon一覧</a></li>
+        <li><a href="edit.php">dogmon育成</a></li>
+        <li><a href="test.php">テストページ</a></li>
+      </ul>
+    </div>
   </div>
 
 </body>
