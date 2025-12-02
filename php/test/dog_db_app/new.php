2785efc7a3c2b658dd9d482be69f7ddd6b3ecce4
tomo-john <keririnkery.kery@gmail.com>
2025-12-02 11:17:33 +0900

    test

--- php/test/dog_db_app/new.php
+++ php/test/dog_db_app/new.php
@@ -29,11 +29,19 @@ if (!empty($_SESSION['errors'])) {
 
     <h3>NEW</h3>
 
-    <div class="menu-list">
-      <a class="link-btn" href='new.php'>作成画面</a>
-      <a class="link-btn" href='list.php'>一覧画面</a>
-      <a class="link-btn" href='test.php'>テストページ</a>
+    <div class="form">
+      <form action="" method="post">
+        <label for="name">名前: </label>
+        <input id="name" name="name" type="text" placeholder="犬の名前を入力してね🐾" value="">
+        <label for="age">年齢: </label>
+        <input id="age" name="age" type="text" placeholder="犬の年齢を入力してね🐾" value="">
+        <input class="link-btn submit-btn" type="submit" value="登録🐶">
+      </form>
     </div>
+    <div class="menu-list">
+      <a class="link-btn" href='index.php'>戻る🐶</a>
+    </div>
+
   </div>
 
 </body>
