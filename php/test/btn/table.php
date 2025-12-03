7ff93d9dc637030542362b2ce46825ed85477ca1
tomo-john <keririnkery.kery@gmail.com>
2025-12-04 06:31:52 +0900

    test

--- php/test/btn/table.php
+++ php/test/btn/table.php
@@ -1,7 +1,7 @@
 <?php
 // table.php
 $num = $_POST['num'] ?? '';
-var_dump($num);
+$max = 10;
 
 ?>
 
@@ -19,15 +19,40 @@ var_dump($num);
 
     <h3>🐶テーブル🐶</h3>
     
-    <form method="post">
-      <label for="num">数値を選んでね🐶:</label>
-      <select id="num" name="num">
-        <?php for($i=1; $i<=5; $i++ ): ?>
-          <option value="<?=$i ?>"><?php echo $i ?></option>
-        <?php endfor; ?>
-      </select>
-      <input type="submit" value="作成🐶">
-    </form>
+    <div class="input">
+      <form method="post">
+        <label for="num">数値を選んでね🐶:</label>
+        <select id="num" name="num">
+          <?php for($i = 1; $i <= $max; $i++): ?>
+            <option value="<?=$i ?>"><?php echo $i ?></option>
+          <?php endfor; ?>
+        </select>
+        <input type="submit" value="テーブル作成🐶">
+      </form>
+    </div>
+
+    <div class="table">
+      <?php if(!empty($num)): ?>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>power</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php for($i = 1; $i <= intval($num); $i++): ?>
+              <tr>
+                <td><?php echo $i; ?></td>
+                <td><?php echo 'じょん' . $i; ?></td>
+                <td><?php echo $i * $i; ?></td>
+              </tr>
+            <?php endfor; ?>
+          </tbody>
+        </table>
+      <?php endif; ?>
+    </div>
 
     <a class="btn" href="index.php">戻る🐶</a>
 
