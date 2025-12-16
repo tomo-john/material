316a457792d7cc2ac667d13b27db64962b46ffd2
tomo-john <keririnkery.kery@gmail.com>
2025-12-16 11:58:55 +0900

    test

--- php/test/index.php
+++ php/test/index.php
@@ -1,3 +1,20 @@
+<?php
+
+// compact()メソッド
+// 変数名とその値から配列を作成
+
+$name = 'じょん';
+$age = '2';
+$breed = 'ゴル';
+$favarite_food = 'やきにく';
+
+$data = compact('name', 'age', 'breed', 'favarite_food');
+
+var_dump($data);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -6,9 +23,8 @@
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <?php $members = ['じょん', 'ぴょんきち', 'もこもか']; ?>
-  <?php foreach ($members as $m): ?>
-    <li><?= $m ?></li>
+  <?php foreach ($data as $d): ?>
+    <li><?= $d ?></li>
   <?php endforeach; ?>  
 </body>
 </html>
